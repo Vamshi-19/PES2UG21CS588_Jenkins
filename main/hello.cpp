89afc02bb9c9c1ca5c176cb24411c748af2ed1f1
vamshi19 <vamshi.gowda19@gmail.com>
2024-03-10 09:31:15 +0530

    Add a new line to hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "This line was added or modified.\n"; // Add this line
   return 0;
 }
